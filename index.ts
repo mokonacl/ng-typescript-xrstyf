a7cbc024975c449dcf3075d84dc8ef9e8dde8fe4
mokonacl <mokonacl@gmail.com>
2023-04-23 12:06:01 +0900

    2 files modified, 1 added

--- index.ts
+++ index.ts
@@ -1,48 +1,12 @@
-let hoge: string = 'hoge';
-let foo: string = 'foo';
-let count: number = 10;
-let isFoo: boolean = foo === 'foo';
-let payment = {
-  amount: 3000,
-  currency: 'JPY',
-  name: 'charge',
-  date: 1596603007427,
-  taxRate: 0.1,
-  total: 0
-};
+var title: string = 'Angular';
+title = 'ANGULAR !!';
 
-// Function
-function log(message: string): void {
-  console.log(message);
-}
-log('Hello, TypeScript!!');
+var n: number = 99;
+var isOpen: boolean = true;
 
-const getTotal = () => {
-  const { amount, taxRate } = payment;
-  const total = amount * taxRate;
+var keywords: (string | number)[] = ['name', 'email', 'body', 0];
 
-  return { total, ...payment };
-};
+var paydata: [number, string, number] = [77, 'taro', 88];
 
-// Generic
-class Store<T> {
-  data: T | null = null;
-  posts: Post[] = [];
-  getData(): T|null {
-    return this.data;
-  }
-}
+var obj: { [key: string]: string } = { name: 'sato', id: 'test' };
 
-
-let stringData = new Store<string>();
-
-
-// Interface
-interface Post {
-  id: number;
-  content: string;
-  isDraft: boolean;
-  attachments: object;
-  created: string;
-  updated: string;
-}
